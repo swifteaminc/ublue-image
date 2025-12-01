121a1d746d48c6b83a59285608818c02dcefe2ad
Harsh Sharma <hsharma0824@gmail.com>
2025-12-02 01:17:34 +0530

    Fix GPG check and clean up install script
    Updated the installation script to enable GPG check and modified comments for clarity.

--- build_files/install_swifteam.sh
+++ build_files/install_swifteam.sh
@@ -1,31 +1,14 @@
-# sudo tee /etc/yum.repos.d/swifteam.repo > /dev/null <<'EOF'
-# [swifteam]
-# name=Swifteam Repository
-# baseurl=https://swif-linux-package.s3.amazonaws.com/fedora/main/x86_64
-# enabled=1
-# gpgcheck=1
-# repo_gpgcheck=1
-# gpgkey=https://swif-linux-package.s3.amazonaws.com/RPM-GPG-KEY-swifteam.pub
-# EOF
-
-# 1) Add repo
-tee /etc/yum.repos.d/swifteam.repo >/dev/null <<'EOF'
+sudo tee /etc/yum.repos.d/swifteam.repo > /dev/null <<'EOF'
 [swifteam]
 name=Swifteam Repository
 baseurl=https://swif-linux-package.s3.amazonaws.com/fedora/main/x86_64
 enabled=1
 gpgcheck=1
-repo_gpgcheck=0
+repo_gpgcheck=1
 gpgkey=https://swif-linux-package.s3.amazonaws.com/RPM-GPG-KEY-swifteam.pub
 EOF
 
-# 2) Import key non-interactively
-rpm --import https://swif-linux-package.s3.amazonaws.com/RPM-GPG-KEY-swifteam.pub
 
-# 3) TEMP: relax digest verification for Fedora 43
-cat >/etc/rpm/macros.d/macros.swifteam <<'EOF'
-%_pkgverify_level none
-EOF
 
 sudo dnf makecache
 sudo dnf install swifteam -y
